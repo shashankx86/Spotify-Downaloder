2506a1ca46992b03c87c9bba761432003755e954
Anshul Kumar Gupta <83653522+AnshulDev21@users.noreply.github.com>
2021-10-24 17:45:23 +0530

    Update qt_core.py

--- qt_core.py
+++ qt_core.py
@@ -4,4 +4,5 @@ import time
 from PySide2 import QtCore
 from PySide2.QtCore import QRect, QCoreApplication, QMetaObject, QThread, QProcess, Qt
 from PySide2.QtGui import QPixmap, QColor
-from PySide2.QtWidgets import QWidget, QLabel, QPushButton, QComboBox, QLineEdit, QMainWindow, QApplication, QGraphicsDropShadowEffect, QFileDialog
+from PySide2.QtWidgets import QWidget, QLabel, QPushButton, QComboBox, QLineEdit, QMainWindow, QApplication, QGraphicsDropShadowEffect
+from PySide2.QtWidgets import QFileDialog
